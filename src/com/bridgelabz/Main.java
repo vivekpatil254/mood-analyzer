8c233facf407eb3fe549b6a631bed9fc35a70217
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 <Vivekpatil254@gmail.com>
2022-05-15 23:03:56 +0530

    first commit

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Ability to analyze and respond happy or sad mood ");
+        MoodAnalyzer moodAnalyzer=new MoodAnalyzer();
+
+    }
+}
---
+++ src/com/bridgelabz/MoodAnalyzer.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class MoodAnalyzer {
+    public void main(String message)
+    {
+
+
+
+    }
+
+
+}
